eb4259ad6d580d4f064488a68d92ff1028b05191
damblatt <dam.blatt@gmail.com>
2022-10-25 17:03:45 +0200

    camel case

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,18 +2,18 @@ class searchView {
   // Suchfunktion (filterfunktion)
   searchFunction() {
     document
-      .querySelector(".search__field")
-      .addEventListener("input", function () {
-        let query = document.querySelector(".search__field").value;
-        const items = document.querySelectorAll(".name");
+      .querySelector('.searchField')
+      .addEventListener('input', function () {
+        let query = document.querySelector('.searchField').value;
+        const items = document.querySelectorAll('.name');
 
         // nur karten welche den suchbegriff beinhalten werden gerendert
         items.forEach((i) => {
           if (!i.innerHTML.toLowerCase().includes(query.toLowerCase())) {
-            i.parentElement.style.display = "none";
+            i.parentElement.style.display = 'none';
           } else {
             i.innerHTML.indexOf;
-            i.parentElement.style.display = "inherit";
+            i.parentElement.style.display = 'inherit';
           }
         });
       });
