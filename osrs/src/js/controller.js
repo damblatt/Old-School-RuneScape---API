1e38def9319db4897fd9ebfe4147f8945af518de
damblatt <dam.blatt@gmail.com>
2022-11-18 12:02:47 +0100

    commit

---
+++ osrs/src/js/controller.js
@@ -0,0 +1,19 @@
+import model from './model.js';
+
+model.loadCards();
+
+//document.addEventListener('input', searchView.searchFunction);
+/*
+https://prices.runescape.wiki/api/v1/osrs/mapping
+Format
+----------
+examine: "A fiery glow emanates from this necklace."
+highalch: 121200
+icon: "Zenyte necklace.png (synthax: https://oldschool.runescape.wiki/images/Zenyte_necklace.png)
+"id: 19535
+limit: 8
+lowalch: 80800
+members: true
+name: "Zenyte necklace"
+value: 202000
+*/
---
+++ osrs/src/js/model.js
@@ -0,0 +1,56 @@
+class model {
+  loadCards() {
+    async function grab(url) {
+      const res = await fetch(url);
+      const json = await res.json();
+      return json;
+    }
+
+    const list = document.querySelector('.itemCards');
+
+    grab('https://prices.runescape.wiki/api/v1/osrs/mapping').then((data) => {
+      data.forEach((item) => {
+        const imgLink = `https://oldschool.runescape.wiki/images/${item.icon.replaceAll(
+          ' ',
+          '_'
+        )}`;
+        const e = list.insertAdjacentHTML(
+          'beforeend',
+          `
+              <div class="card">
+                <div class="cardContent">
+                  <p class="name">${item.name}</p>
+                  <div class="info">
+                      <img class="icon" src="${imgLink}">
+                    <p class="examine">${item.examine}</p>
+                  </div>
+                </div>
+                <div class="delete">
+                  <button class="delete-button">bruv</button>
+                </div>
+              </div>
+              `
+        );
+      });
+    });
+
+    // Suchfunktion (filterfunktion)
+    document
+      .querySelector('.searchField')
+      .addEventListener('input', function () {
+        let query = document.querySelector('.searchField').value;
+        const items = document.querySelectorAll('.name');
+
+        // nur karten welche den suchbegriff beinhalten werden gerendert
+        items.forEach((i) => {
+          if (!i.innerHTML.toLowerCase().includes(query.toLowerCase())) {
+            i.parentElement.style.display = 'none';
+          } else {
+            i.innerHTML.indexOf;
+            i.parentElement.style.display = 'inherit';
+          }
+        });
+      });
+  }
+}
+export default new model();
