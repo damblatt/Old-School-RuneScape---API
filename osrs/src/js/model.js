7771e06e0a55a11531e1e10ba2c1876a54846ba9
damblatt <dam.blatt@gmail.com>
2022-11-22 22:37:28 +0100

    commit

--- osrs/src/js/model.js
+++ osrs/src/js/model.js
@@ -19,9 +19,12 @@ class model {
           `
               <div class="card">
                 <div class="cardContent">
-                  <p class="name">${item.name}</p>
+                  <div display="inline">
+                    <p class="name" style="display:inline">${item.name}</p>
+                    <input class=" " type="image" src="src/img/bookmark-white.png" height="24px width="auto" style="float:right">
+                  </div>
                   <div class="info">
-                      <img class="icon" src="${imgLink}">
+                    <img class="icon" src="${imgLink}">
                     <p class="examine">${item.examine}</p>
                   </div>
                   <button class="deleteButton buttonSettings changeAndDeleteButtonSettings">Delete</button>
@@ -52,35 +55,44 @@ class model {
       });
 
     // Change Button
-    function namePrompt(e) {
+    function namePrompt() {
       let name = prompt('Item name:');
-      if (name == null || name == '') {
-        console.log('Invalid name');
-      } else {
-        console.log(name);
-        descriptionPrompt(e, name);
-      }
+      console.log(name);
+      return name;
     }
-    function descriptionPrompt(e) {
+    function descriptionPrompt() {
       let description = prompt('Item description:');
-      if (description == null || description == '') {
-        console.log('Invalid description');
-      } else {
-        console.log(description);
-        
-        console.log('Item changed');
-      }
+      console.log(description);
+      return description;
     }
+    function changeData(e, name, description) {
+      deleteCard(e);
+      addCard(name, description);
+      console.log('Item name/description successfully changed');
+    }
+
     list.addEventListener('click', (e) => {
       if (e.target.classList.contains('changeButton')) {
-        namePrompt(e);
+        let name = namePrompt();
+        if (name == null || name == '') {
+          console.log('Invalid name');
+        } else {
+          let description = descriptionPrompt();
+          if (description == null || description == '') {
+            console.log('Invalid description');
+          } else {
+            changeData(e, name, description);
+          }
+        }
       }
     });
 
-    // Delete Button
+    // DeleteCard
     function deleteCard(e) {
       e.target.parentElement.parentElement.remove();
     }
+
+    // DeleteCardButton
     list.addEventListener('click', (e) => {
       if (e.target.classList.contains('deleteButton')) {
         console.log('Item removed');
@@ -88,31 +100,82 @@ class model {
       }
     });
 
-    // Add Button
-    document.querySelector('.addButton').addEventListener('click', function () {
-      let itemName = document.querySelector('.newItemName').value;
-      let itemDescription = document.querySelector('.newItemDescription').value;
-      if (itemName && itemDescription) {
-        list.insertAdjacentHTML(
-          'beforeend',
-          `
+    // AddCard
+    function addCard(itemName, itemDescription) {
+      list.insertAdjacentHTML(
+        'beforeend',
+        `
               <div class="card">
                 <div class="cardContent">
-                  <p class="name">${itemName}</p>
+                  <div display="inline">
+                    <p class="name" style="display:inline">${itemName}</p>
+                    <input type="image" src="src/img/bookmark-white.png" height="24px width="auto" style="float:right">
+                  </div>
                   <div class="info">
                     <p class="examine">${itemDescription}</p>
                   </div>
-                  <button class="deleteButton buttonSettings">Delete</button>
-                  <button class="changeButton buttonSettings">Change item data</button>
+                  <button class="deleteButton buttonSettings changeAndDeleteButtonSettings">Delete</button>
+                  <button class="changeButton buttonSettings changeAndDeleteButtonSettings">Change item data</button>
                 </div>
               </div>
               `
-        );
-        console.log('Item added');
+      );
+      console.log('Item added');
+    }
+
+    // AddCardButton
+    document.querySelector('.addButton').addEventListener('click', function () {
+      let itemName = namePrompt();
+      let itemDescription = descriptionPrompt();
+      if (
+        itemName != null &&
+        itemName != '' &&
+        itemDescription != null &&
+        itemDescription != ''
+      ) {
+        addCard(itemName, itemDescription);
       } else {
         console.log('Item not added');
       }
     });
+
+    // HomeButton
+    document.querySelector('.logoImage').addEventListener('click', function () {
+      const items = document.querySelectorAll('.name');
+      console.log(items);
+      items.forEach((i) => {
+        i.parentElement.parentElement.style.display = 'inherit';
+      });
+      document.querySelector('.searchField').value = '';
+    });
+
+    // Bookmark/Unmark
+    list.addEventListener('click', (e) => {
+      if (!e.target.classList.contains('bookmarked')) {
+        e.target.classList.add('bookmarked');
+        e.target.src = 'src/img/bookmark.png';
+      } else {
+        e.target.classList.remove('bookmarked');
+        e.target.src = 'src/img/bookmark-white.png';
+      }
+    });
+
+    // FilterBookmarked
+    document
+      .querySelector('.bookmarkCollection')
+      .addEventListener('click', function () {
+        const items = document.querySelectorAll('.name');
+        items.forEach((i) => {
+          console.log(i.parentElement);
+          let bool =
+            i.innerHTML.target.parentElement.classList.contains('bookmarked');
+          if (bool) {
+            i.style.display = 'none';
+          } else {
+            i.style.display = 'inherit';
+          }
+        });
+      });
   }
 }
 export default new model();
